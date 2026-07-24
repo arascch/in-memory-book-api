d6eb3f9dd2c04afd514f2ebad5dc8daa5d68fc77
arasch <arasch.ask@gmail.com>
2026-07-24 10:19:43 +0330

    add create book func

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI , HTTPException,status
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Book(BaseModel):
+    title:str
+    author : str
+    price : float
+
+books_db:dict[int,dict]={}
+next_id = 1
+
+@app.post("/books" , status_code=status.HTTP_201_CREATED)
+def create_book(book: Book):
+    global next_id
+    book_data = book.dict()
+    book_data["id"]=next_id
+    books_db[next_id] = book_data
+    next_id +=1
+    return book_data
+
