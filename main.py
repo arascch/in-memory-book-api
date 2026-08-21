719c02e91e541a507de99c56955d44126fdf940e
arasch <arasch.ask@gmail.com>
2026-08-21 11:29:22 +0330

    token and user validation func

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def create_access_token(data:dict)->str:
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY , algorithm=ALGORITHM)
 
+    
 
 sqlite_file_name = "bookstore.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -74,6 +75,26 @@ def get_book_or_404(book_id:int , session: Session= Depends(get_session)):
     if book is None:
         raise HTTPException(status_code=404,detail="book not found")
     return book
+
+def get_current_user(token:str=Depends(oauth2_scheme) , session: Session = Depends(get_session)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail= "Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token , SECRET_KEY , algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except jwt.JWTError:
+        raise credentials_exception
+
+    user = session.exec(select(User).where(User.username == username)).first()
+    if user is None:
+        raise credentials_exception
+    return user
+
 
 @app.post("/login")
 def login(form_data: OAuth2PasswordRequestForm = Depends() , session:Session=Depends(get_session)):
